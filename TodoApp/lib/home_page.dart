6325b22220a0b4cbb25060ff4317267093351793
harshad diwate <harshaddiwate85@gmail.com>
2026-01-23 01:47:49 +0530

    feat:create basic todotile and home page structure

--- TodoApp/lib/home_page.dart
+++ TodoApp/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/utils/todo_tile.dart';
 
 class home_page extends StatefulWidget {
    home_page({super.key});
@@ -14,12 +15,29 @@ class _home_pageState extends State<home_page> {
 backgroundColor: Colors.yellow[300],
       appBar: AppBar(
         centerTitle: true,
-title: Text("TO DO"),
+title: Text("TODO APP 📝"),
         elevation: 0,
       ),
       body: ListView(
-        children: [],
+        padding: EdgeInsets.all(25),
+
+        children: [
+          TodoTile(taskname: 'Buy Groceries 🍅', isChecked: true,),
+          TodoTile(taskname: 'Do Homework 📚', isChecked: false,),
+          TodoTile(taskname: 'Study Flutter ❤️‍🔥', isChecked: true,),
+          TodoTile(taskname: 'Drink Whiskey 🍺', isChecked: false,),
+          TodoTile(taskname: 'Go to gym 💪', isChecked: true,),
+          TodoTile( taskname: ' Do Programming 🧑‍🏫', isChecked: false,),
+
+
+
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){print("TODO added Sucessfully");},
+        child: Icon(Icons.add),
       ),
     );
   }
 }
+
--- TodoApp/lib/utils/todo_tile.dart
+++ TodoApp/lib/utils/todo_tile.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 
-class todo_tile extends StatefulWidget {
-  const todo_tile({super.key});
+class TodoTile extends StatelessWidget {
+ final String taskname ;
+ final bool isChecked;
+Function (bool?)? onChanged;
 
-  @override
-  State<todo_tile> createState() => _todo_tileState();
-}
+   TodoTile({super.key, required this.taskname, required this.isChecked});
 
-class _todo_tileState extends State<todo_tile> {
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+
+      margin: EdgeInsets.only(top:20),
+      height: 100,
+      width: 100,
+      padding: EdgeInsets.all(25),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.grey[200],
+
+      ),
+      child: Row(
+        children: [
+          Checkbox(value: isChecked, onChanged: (value) {}),
+          SizedBox(width: 20,),
+          Text(taskname,style: TextStyle(fontSize: 20,decoration: isChecked ? TextDecoration.lineThrough : null)),
+
+        ],
+      ),
+    );;
   }
 }
